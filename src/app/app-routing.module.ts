debc26d2c799185db5d12e197d0b0f3cf7b26722
jparba <jbparba@asscat.edu.ph>
2023-02-06 23:52:49 +0800

    add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: LoginComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      // {
+      //   path: 'logout',
+      //   component: ,
+      // },
+      // {
+      //   path: 'request-password',
+      //   component: ,
+      // },
+      // {
+      //   path: 'reset-password',
+      //   component: ,
+      // },
+    ],
+  },
+  { path: '', redirectTo: 'auth', pathMatch: 'full'},
+  { path: '**', redirectTo: 'auth' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
